cbd79c1285d1f1d2c5b6f9d0f4d6db9cd9c4c818
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2022-07-14 20:58:47 -0300

    register routers into express

--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,33 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+// Routes
+const productsRouter = require("./routers/products");
+const productRouter = require("./routers/product");
+const usersRouter = require("./routers/users");
+const categoriesRouter = require("./routers/categories");
 
+// Connecting to MongoDB
 mongoose.connect("mongodb://localhost:27017/fluffshop")
     .then(() => console.log('Conectou ao mongodb'))
     .catch(e => console.error(e));
 
+// Instantiating App
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cors());
 
-// Routes
+// Registering Routers
+app.use("/products", productsRouter);
+app.use("/product", productRouter);
+app.use("/users", usersRouter);
+app.use("/categories", categoriesRouter);
 
 
 // Start app
-const port = 3001;
+const port = 3000;
 const server = app.listen(
     port, 
-    () => console.log(`Listening on localhost:${port}`)
+    () => console.log(`Listening on http://localhost:${port}`)
 );

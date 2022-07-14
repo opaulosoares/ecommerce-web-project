3d23af910554e21811c2ba7a5b692f1f68d839ad
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2022-07-14 20:56:29 -0300

    add /users router and fetchUsers GET method

---
+++ backend/routers/users.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const User = require("../models/user");
+const {default: mongoose } = require("mongoose");
+
+const router = express.Router();
+
+// Get All Users
+router.get('/', async(req, res) => {
+    res.status(200).send(
+        await User.find({})
+    )
+});
+
+module.exports = router;

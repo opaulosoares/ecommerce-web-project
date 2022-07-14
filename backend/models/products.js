2da1622000907378bc51d48e577c3f1bb32276cd
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2022-07-14 10:22:00 -0300

    add products schema

---
+++ backend/models/products.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+    id: mongoose.Schema.Types.ObjectId,
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String
+    },
+    petSpecie: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    petAge: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    onStock: {
+        type: Number,
+        required: true,
+    }
+})
+
+const Product = mongoose.model("Product", schema);
+module.exports = Product;

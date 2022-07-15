e5d04bf0fa5363a038c7718bcde749040cd56fad
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2022-07-15 11:28:29 -0300

    add delete product by _id DELETE method

--- backend/routers/product.js
+++ backend/routers/product.js
@@ -1,22 +1,34 @@
 const express = require("express");
 const Product = require("../models/product");
-const {default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
 const router = express.Router();
 
 const INVALID_ID = "Invalid ObjectID";
 const PRODUCT_NOT_FOUND = "Product not found";
 
-// Get All Categories
+// Get Product by ID
 router.get('/:id', async(req, res) => {
     if (!mongoose.isValidObjectId(req.params.id))
-        return res.status.apply(400).send({error: INVALID_ID})
+        return res.status(400).send({error: INVALID_ID});
     
     const targetProduct = await Product.findById(req.params.id).exec();
-    if (!targetProduct)
-        return res.status(404).send(PRODUCT_NOT_FOUND);
+    if (targetProduct === null)
+        return res.status(404).send({error: PRODUCT_NOT_FOUND});
     
-    res.status(200).send(targetProduct);
+    return res.status(200).send(targetProduct);
+});
+
+// Deletes one Product by ID
+router.delete('/:id', async(req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id))
+        return res.status(400).send({error: INVALID_ID});
+    
+    const targetProduct = await Product.findByIdAndDelete(req.params.id).exec();
+    if (targetProduct === null)
+        return res.status(404).send({error: PRODUCT_NOT_FOUND});
+    
+    return res.status(200).send();
 });
 
 module.exports = router;

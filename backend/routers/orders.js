f6f56f9f0b7ef3d141ce445343ebafa4ee22b4f2
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2022-07-15 20:45:42 -0300

    add GET method to get a Order by ID

--- backend/routers/orders.js
+++ backend/routers/orders.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const Order = require("../models/order");
 const Product = require("../models/product");
+const mongoose = require("mongoose");
 
 const router = express.Router();
+
+const INVALID_ID = "Invalid ObjectID";
+const PRODUCT_NOT_FOUND = "Order not found";
 
 // Get all Orders
 router.get('/', async(req, res) => {
@@ -28,6 +32,18 @@ router.post('/', async (req, res) => {
     } catch (e) {
         return res.status(400).send({error: e});
     }
+});
+
+// Get Order by ID
+router.get('/:id', async(req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id))
+        return res.status(400).send({error: INVALID_ID});
+    
+    const targetOrder = await Order.findById(req.params.id).exec();
+    if (targetOrder === null)
+        return res.status(404).send({error: PRODUCT_NOT_FOUND});
+    
+    return res.status(200).send(targetOrder);
 });
 
 module.exports = router;
